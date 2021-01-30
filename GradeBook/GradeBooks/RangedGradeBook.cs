fa7b3c7188c4ca4054c316c68710581b6048656d
David Johansen <davidjohansenakv@gmail.com>
2021-01-30 13:58:53 +0100

    Added an enum type and Implemented some classes inherting from the base gradebook class

---
+++ GradeBook/GradeBooks/RangedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RangedGradeBook : BaseGradeBook
+    {
+        public RangedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked; 
+        }
+
+    }
+}
